3659121007b856ad50349cec36669ad820992cbe
Denisd28 <dsalmerono14@gmail.com>
2023-08-27 08:56:35 -0600

    Detalles del formulario

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
                         </div>
                         <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Recuerdarme</label>
                     </div>
-                    <button type="submit" className=" button text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesion</button>
+                    <button type="submit" className=" buttonLogin text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesion</button>
                 </form>
             </div>
         </div>
