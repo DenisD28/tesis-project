e02b449779de48c6f3b331a44b6a64d7d11ad3a7
dunisd28 <dsalmerono14@gmail.com>
2023-09-17 15:40:25 -0600

    Creacion de vista de productos terminados

--- src/pages/Inventarios/SalidaDeMateriaPrima.tsx
+++ src/pages/Inventarios/SalidaDeMateriaPrima.tsx
@@ -2,6 +2,27 @@ import { useEffect, useState } from "react"
 import { invent, listProduct } from "../../Components/types.d"
 import { agregarProductoTerminado, listaInventario } from "../../services/Services"
 import { useNavigate } from "react-router-dom"
+import { HeadType } from "../../Components/Table/types/HeadType"
+import Head from "../../Components/Table/Head/Head"
+import { Menu } from "../../routes/Menu"
+import { Nav } from "../../Components/Navegador/Nav"
+import InputsForm from "../../Components/Forms/InputsComponents/InputsForm"
+import ButtonForm from "../../Components/Forms/ButtonComponents/ButtonForm"
+
+const headers: HeadType[] = [
+    { name: "Codigo", prop: "codigo" },
+    { name: "Nombre", prop: "nombre" },
+    { name: "Stock", prop: "stock" },
+    { name: "Accion", prop: "accion" }
+]
+
+const secondheaders: HeadType[] = [
+    { name: "Cantidad", prop: "cantidad" },
+    { name: "Nombre", prop: "nombre" },
+]
+
+const titleTable = 'Materia Prima'
+const titleTable2 = 'Materia Prima Usada'
 
 export const SalidaDeMateriaPrima: React.FC = () => {
     const [cantidad, setCantidad] = useState("")
@@ -51,90 +72,112 @@ export const SalidaDeMateriaPrima: React.FC = () => {
         }
     }
 
-    const cancelar = () => {
-    }
-
     return (
         <>
-            <div className="container_total">
-                <div className="">
-                    <h2 className="titulo">Agregar Productos Usados</h2>
-                    <br />
-                    <div className="relative">
-                        <label htmlFor="floating_outlined" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad a utilizada</label>
-                        <input name="description" value={cant} onChange={(e) => setCant(e.target.value)} type="number" id="floating_outlined" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cantidad a ingresar " required />
+            <div className="contenedor">
+                <div className="div-menu">
+                    <Menu />
+                </div>
+                <div className="div-dashboard">
+                    <div>
+                        <Nav />
                     </div>
-                    <div className="tabla_materia">
-                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-                                    <tr>
-                                        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                            Codigo
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Nombre
-                                        </th>
-                                        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                            Stock
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Disponibilidad
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Cantidad Necesitada
-                                        </th>
-                                        <th scope="col" className="px-6 py-3">
-                                            Acciones
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        product.map(pro => (
-                                            <tr key={pro.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                                <td className="px-6 py-4">{pro.code}</td>
-                                                <td className="px-6 py-4">{pro.product}</td>
-                                                <td className="px-6 py-4">{pro.stock}</td>
-                                                <td className="px-6 py-4">{pro.status}</td>
-                                                <td className="px-6 py-4">
-                                                    <button onClick={() => agregar(pro.id)}>Agregar</button>
-                                                </td>
-                                            </tr>
-                                        ))
-                                    }
-                                </tbody>
-                            </table>
+                    <div className="contenido">
+                        <div className="form-salida">
+                            <div className="column1">
+                                <div className="card">
+                                    <p><span>M-005</span></p>
+                                    <div className="producto">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35"><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z" /></svg>
+                                        <h1>Pan Simple</h1>
+                                    </div>
+                                    <p>15 bolsas</p>
+                                </div>
+                                {/* primeratabla */}
+                                <div className='px-8 rounded-xl bg-white md:h-96 h-80 overflow-y-auto hidden-scroll shadow-lg shadow-[#ddd] border-2'>
+                                    <h1 className='sm:text-2xl text-lg font-bold my-4 h-16 w-full sticky top-0 left-0 bg-white pt-4 text-[#4F46E5]'>{titleTable}</h1>
+                                    <table className='w-full h-full'>
+                                        <Head headers={headers} />
+                                        <tbody>
+                                            {product.map((dat, index) => (
+                                                <tr
+                                                    key={index}
+                                                    className='border-b-[1px] border-[#eee] h-14 sm:h-12'
+                                                >
+                                                    {headers.map((h, i) => (
+                                                        <td
+                                                            key={i}
+                                                            className='text-[#3d333a]/90 text-center font-base sm:text-base text-sm'>
+                                                            {
+                                                                dat[h.prop]
+                                                            }
+                                                        </td>
+                                                    ))}
+                                                    <td className="px-6 py-4">
+                                                        <button onClick={() => agregar(dat.id)}>Agregar</button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            {/* SegundaTabla */}
+                            <div className="column2">
+                                <div className='px-8 rounded-xl bg-white md:h-96 h-80 overflow-y-auto hidden-scroll shadow-lg shadow-[#ddd] border-2'>
+                                    <h1 className='sm:text-2xl text-lg font-bold my-4 h-16 w-full sticky top-0 left-0 bg-white pt-4 text-[#4F46E5]'>{titleTable2}</h1>
+                                    <table className='w-full h-full'>
+                                        <Head headers={secondheaders} />
+                                        <tbody>
+                                            {list.map((dat, index) => (
+                                                <tr
+                                                    key={index}
+                                                    className='border-b-[1px] border-[#eee] h-14 sm:h-12'
+                                                >
+                                                    <td>
+                                                        <InputsForm DataInputs={{
+                                                            'title': '',
+                                                            'type': 'number',
+                                                            'name': 'cantidad',
+                                                            'placeholder': 'Escribe el nombre de la organizacion',
+                                                        }} />
+                                                    </td>
+                                                    {headers.map((h, i) => (
+                                                        <td
+                                                            key={i}
+                                                            className='text-[#3d333a]/90 text-center font-base sm:text-base text-sm'>
+                                                            {
+                                                                dat[h.prop]
+                                                            }
+                                                        </td>
+                                                    ))}
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <form action="" className="form-salidaMP">
+                                    <ButtonForm dataButton={{
+                                        'title': 'Cancelar',
+                                        'color': 'red',
+                                        'type': 'reset',
+                                    }} />
+                                    <ButtonForm dataButton={{
+                                        'title': 'Guardar',
+                                        'color': 'green',
+                                        'type': 'submit',
+                                    }} />
+                                </form>
+
+                            </div>
+
+
+
                         </div>
+
                     </div>
                 </div>
-                <div className=" contenedor">
-                    <div className="form_2">
-                        <form onSubmit={(e) => handleSubmit(e)}>
-                            <div className="relative" >
-                                <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Id producto</label>
-                                <label htmlFor="floating_outlined" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{id}</label>
-                            </div>
-                            <div className="relative">
-                                <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre Producto</label>
-                                <label htmlFor="floating_outlined" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{nombre}</label>
-                            </div>
-                            <div className="relative">
-                                <label htmlFor="floating_outlined" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad a ingresar</label>
-                                <input name="description" value={cantidad} onChange={(e) => setCantidad(e.target.value)} type="number" id="floating_outlined" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cantidad a ingresar " required />
-                            </div>
-                            <div className="buttons">
-                                <div className="button">
-                                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Registrar</button>
-                                </div>
-                                <div className="button">
-                                    <button onClick={cancelar} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancelar</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div >
+            </div>
         </>
     )
 }
