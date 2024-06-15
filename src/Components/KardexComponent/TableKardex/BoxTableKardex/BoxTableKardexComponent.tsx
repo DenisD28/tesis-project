5237affa9754ea7eb7cfe26248daf1d60175e74a
AndresC1 <andrescornejolira@gmail.com>
2024-06-15 13:55:57 -0600

    feat: Avances de la UI para la KardexPage.tsx

--- src/Components/KardexComponent/TableKardex/BoxTableKardex/BoxTableKardexComponent.tsx
+++ src/Components/KardexComponent/TableKardex/BoxTableKardex/BoxTableKardexComponent.tsx
@@ -11,7 +11,7 @@ export default function BoxTableKardexComponent({dat, headers}: {dat: any, heade
             {headers.map((h, i) => (
                 <div
                     key={i}
-                    className='px-6 py-3 flex justify-start items-center'>
+                    className={`px-6 py-3 flex justify-start items-center ${dat["input"]>0 ? 'bg-green-50' : 'bg-red-50'}`}>
                     {h.prop === 'product' ? (dat[h.prop] as { name: string }).name : dat[h.prop]}
                 </div>
             ))}
@@ -19,7 +19,12 @@ export default function BoxTableKardexComponent({dat, headers}: {dat: any, heade
                 <div
                     className='px-6 py-3 text-center border-t-2 col-span-4'
                 >
-                    <BoxShowInformationComponent title={"Cliente"} value={"Andres"}/>
+                    <BoxShowInformationComponent title={"Registrado por"} value={"Andres Alexander Cornejo Lira"}/>
+                    <BoxShowInformationComponent title={"Fecha"} value={"2021-10-10"}/>
+                    <BoxShowInformationComponent title={"Descripción"} value={"Compra de materia prima a proveedor Quitzon, Schaefer and Ritchie. Fact No.3123 el 11-06-2024."}/>
+                    <BoxShowInformationComponent title={"Cantidad"} value={"100"}/>
+                    <BoxShowInformationComponent title={"Valor Unitario"} value={"C$ 100.00"}/>
+                    <BoxShowInformationComponent title={"Valor Total"} value={"C$ 10000.00"}/>
                 </div>
             )}
         </div>
