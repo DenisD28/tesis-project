ebec55b32989a9c771673902ea0d0c363d665a61
AndresC1 <andrescornejolira@gmail.com>
2024-11-04 11:14:20 -0600

    fix: Ajustes de la UI

--- src/Components/Produccion/FormProd/FormProd.tsx
+++ src/Components/Produccion/FormProd/FormProd.tsx
@@ -1,92 +1,95 @@
 import ButtonForm from '../../Forms/ButtonComponents/ButtonForm'
-import { Receipt } from 'lucide-react'
-import { FormProdProps } from './FormProdPropsTypes'
-import { useEffect, useState } from 'react'
-import { inventario, Product } from '../../types.d'
-import { listaProductoTerminado } from '../../../services/Products/ListaInventarioPTServices'
+import {Receipt} from 'lucide-react'
+import {FormProdProps} from './FormProdPropsTypes'
+import {useEffect, useState} from 'react'
+import {inventario, Product} from '../../types.d'
+import {listaProductoTerminado} from '../../../services/Products/ListaInventarioPTServices'
 import InputsForm from '../../Forms/InputsComponents/InputsForm'
-import { ModalProductoTP } from '../../Modal/ModalProducto2/ModalProductoTP'
+import {ModalProductoTP} from '../../Modal/ModalProducto2/ModalProductoTP'
 
 
-export default function FormProd({ setCodigo, HandleNextOperation, }: FormProdProps) {
-  const [producto, setProducto] = useState<inventario[]>([])
-  const [isOpen, setIsOpen] = useState(false);
-  const [unidadMedida, setUnidadMedida] = useState("")
-  const [id, setId] = useState("0")
-  const [name, setName] = useState("")
+export default function FormProd({setCodigo, HandleNextOperation}: FormProdProps) {
+    const [producto, setProducto] = useState<inventario[]>([])
+    const [isOpen, setIsOpen] = useState(false);
+    const [unidadMedida, setUnidadMedida] = useState("")
+    const [id, setId] = useState("0")
+    const [name, setName] = useState("")
 
 
-  useEffect(() => {
-    lista()
-  }, [])
+    useEffect(() => {
+        lista()
+    }, [])
 
-  const lista = async () => {
-    try {
-      // const { links, meta, clients } = await listaCliente()
-      const { inventario } = await listaProductoTerminado(1)
-      setProducto(inventario)
-    } catch (e) {
-      // console.log(e)
-    }
-  }
-
-  const agregar = (id: inventario) => {
-    setCodigo(id.id.toString())
-    setName(id.product.name)
-    setIsOpen(false)
-  }
-
-  return (
-    <article className="grid grid-cols-12 md:[&>section]:col-span-6 [&>section]:col-span-full border-2 p-4 rounded-md">
-      <h2 className='col-span-12 p-2 text-lg font-medium text-[#000] flex gap-2'>
-        <Receipt />
-        Informaci&oacute;n del producto
-      </h2>
-      <section>
-        {
-          isOpen && (
-            <ModalProductoTP fnAgregar={agregar}
-              setIsOpen={setIsOpen} />
-          )
+    const lista = async () => {
+        try {
+            // const { links, meta, clients } = await listaCliente()
+            const {inventario} = await listaProductoTerminado(1)
+            setProducto(inventario)
+        } catch (e) {
+            // console.log(e)
         }
+    }
+    console.log({
+        inventory_id,
+        name
+    })
 
-        <InputsForm
-          DataInputs={{
-            name: "name",
-            title: "Nombre del Producto",
-            value: name || "",
-            type: "text",
-            placeholder: "Producto a agregar",
-            isRequire: true,
-            isDisabled: true,
-            fnChange: setName,
-          }}
-        />
-        <ButtonForm
-          dataButton={{
-            title: "BuscarProducto",
-            color: "blue",
-            type: "button",
-            fnClick: () => { setIsOpen(true) },
-          }}
-        />
-        {/* <SelectForm dataSelect={{
-          title: 'Producto a Ingresar',
-          name: 'producto',
-          placeholder: 'Seleccione el producto',
-          options: producto.map((item: inventario) => ({ 'valor': item.id, 'texto': item.product.name })),
-          fnChange: setCodigo,
-          isRequerid: true
-        }} /> */}
-      </section>
-      <div className="col-span-12 grid items-center">
-        <ButtonForm dataButton={{
-          title: 'Siguiente',
-          type: 'submit',
-          color: 'blue',
-          fnClick: HandleNextOperation
-        }} />
-      </div>
-    </article>
-  )
+
+    const agregar = (id: inventario) => {
+        setCodigo(id.id.toString())
+        setName(id.product.name)
+        setIsOpen(false)
+    }
+
+    return (
+        <article className="grid grid-cols-12 [&>section]:col-span-full border-2 p-4 rounded-md">
+            <h2 className='col-span-12 p-2 text-lg font-medium text-[#000] flex gap-2'>
+                <Receipt/>
+                Informaci&oacute;n del producto
+            </h2>
+            <section className="col-span-12">
+                {
+                    isOpen && (
+                        <ModalProductoTP fnAgregar={agregar}
+                                         setIsOpen={setIsOpen}/>
+                    )
+                }
+
+                <InputsForm
+                    DataInputs={{
+                        name: "name",
+                        title: "Nombre del Producto",
+                        value: name || "",
+                        type: "text",
+                        placeholder: "Producto a agregar",
+                        isRequire: true,
+                        isDisabled: true,
+                        fnChange: setName,
+                    }}
+                />
+                <ButtonForm
+                    dataButton={{
+                        title: "Buscar producto",
+                        color: "blue",
+                        type: "button",
+                        fnClick: () => {
+                            setIsOpen(true)
+                        },
+                    }}
+                />
+            </section>
+            {
+                name != '' ? (
+                    <div className="col-span-12 grid items-center">
+                        <ButtonForm dataButton={{
+                            title: 'Siguiente',
+                            type: 'submit',
+                            color: 'green',
+                            fnClick: HandleNextOperation
+                        }}/>
+                    </div>
+                ) : ""
+            }
+        </article>
+    )
 }
